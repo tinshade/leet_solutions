b0ec382d807974272cc88a185599c23e22d0504e
Angst-angwalkar <angwalkar.316@gmail.com>
2022-01-23 18:23:02 +0530

    Initializing new Br

--- LinkedListCycle.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-	def hasCycle(self, head):
-		fast,slow = head,head
-		while fast and slow and fast.next:
-			fast = fast.next.next
-			slow = slow.next
-			if slow == fast:
-				return True
-		return False
